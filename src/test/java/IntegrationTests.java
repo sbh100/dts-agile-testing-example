2df13724fbed0877fe9b01834381cba1a9129a55
Samuel Hepworth <19031116@stu.mmu.ac.uk>
2019-10-29 10:11:34 +0000

    Wrote unit test to reproduce issue #1

--- src/test/java/IntegrationTests.java
+++ src/test/java/IntegrationTests.java
@@ -47,6 +47,19 @@ public class IntegrationTests {
         Assert.assertEquals(0, basket.getItems().size());
     }
 
+    @Test
+    public void WhenRemoveItemThenBasketTotalIsCorrect() {
+        Basket basket = new Basket();
+        Menu mainMenu = new MainMenu(basket, new NullTextDisplay());
+        Menu addItemMenu = mainMenu.chooseOption(1);
+        mainMenu = addItemMenu.chooseOption(1);         // Coffee
+        addItemMenu = mainMenu.chooseOption(1);
+        Menu removeItemMenu = mainMenu.chooseOption(2);
+        mainMenu = removeItemMenu.chooseOption(1);         // Coffee
+        
+        Assert.assertEquals(0, basket.getTotalPrice());
+    }
+
     class NullTextDisplay implements TextDisplay {
         public void displayLine(String line) {}
     }
